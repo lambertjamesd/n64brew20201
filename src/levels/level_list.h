812c970bb215d12b376e160f99498111f9a65649
James Lambert <lambertjamesd@gmail.com>
2021-11-18 23:25:48 -0700

    Rerun skelatool after merge

--- src/levels/level_list.h
+++ src/levels/level_list.h
@@ -1,5 +1,7 @@
 DEFINE_LEVEL("Level Template", Level_Template, Mars, 4, 0)
-DEFINE_LEVEL("Boot Camp", Boot_Camp, Mars, 2, 0 | LevelMetadataFlagsCampaign | LevelMetadataFlagsMultiplayer)
+DEFINE_LEVEL("Spaceship Template", Spaceship_Template, Mars, 4, 0)
+DEFINE_LEVEL("Boot Camp", Boot_Camp, Mars, 2, 0 | LevelMetadataFlagsCampaign | LevelMetadataFlagsTutorial)
+DEFINE_LEVEL("The Pit", The_Pit, Mars, 2, 0 | LevelMetadataFlagsCampaign)
 DEFINE_LEVEL("Sojourner", Sojourner, Mars, 4, 0 | LevelMetadataFlagsCampaign)
 DEFINE_LEVEL("The Octogon", The_Octogon, Mars, 4, 0 | LevelMetadataFlagsMultiplayer)
 DEFINE_LEVEL("Course 01", Course_01, Mars, 4, 0 | LevelMetadataFlagsMultiplayer)
